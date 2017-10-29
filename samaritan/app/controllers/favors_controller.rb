aef2b418fdbdfe6562737b190e1bb6ca9207af30
Suvedh Srikanth <suvedhs@gmail.com>
2017-10-29 06:28:29 -0500

    got links to work

---
+++ samaritan/app/controllers/favors_controller.rb
@@ -0,0 +1,21 @@
+class FavorsController < ApplicationController
+  def index
+
+
+  end
+
+  def ques
+
+
+  end
+
+  def gmap
+
+
+  end
+
+  def req
+
+  end
+
+end
--- samaritan/config/routes.rb
+++ samaritan/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'favors#index', as: 'home'
+
+
+  get 'ques' => 'favors#ques', as: 'ques'
+
+  get 'gmap' => 'favors#gmap', as: 'gmap'
+
+  get 'req' => 'favors#req', as: 'req'
+
 end
